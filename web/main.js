9d103339f5a06c7898da461743ebe8471e2953ff
angeltsvetkov <mrproper86@gmail.com>
2015-03-03 09:53:31 +0200

    remove files

--- web/main.js
+++
@@ -1,8 +0,0 @@
-//Trust all certificates
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
-exports.getTemp = function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    res.send(200, '25');
-}
